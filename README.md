67f21f06bacfcc4be73f8d593f12812c59308dc9
Damien Jian (TW) <damein.jian.tw@gmail.com>
2025-08-13 20:22:45 +0800

    modified:   README.md 	new file:   requirements.txt 	new file:   test.py

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 2025NTU-IBM_Group10
+
+python version: 3.11.10
+
+requirements:
+
+numpy==2.3.2
+pennylane==0.42.2
+gymnasium==0.26.1
+torch==2.8.0
---
+++ test.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+import pygame
+import time
+
+seed = 42
+env_name = "FrozenLake-v1"
+env = gym.make(env_name, render_mode="human", is_slippery=False)
+
+env.reset(seed=seed)
+actions_list = [2, 2, 1, 1, 1, 2]
+for action in actions_list:
+    observation, reward, terminated, truncated, info = env.step(action)
+    env.render()
+    # sleep for a short duration to visualize the rendering
+    time.sleep(0.4)
+    if terminated or truncated:
+        env.reset()
+env.close()
+print("Test completed successfully.")
