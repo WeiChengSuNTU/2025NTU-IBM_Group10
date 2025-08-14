d3bd2cc15b73dcbfcb71c31a5de58d640e3f70e5
kalvoada <kalvoada@fit.cvut.cz>
2025-08-14 15:17:01 +0800

    memorybuffer: add length method

--- MemoryBuffer.py
+++ MemoryBuffer.py
@@ -13,7 +13,7 @@ class ReplayBuffer:
         """
         self.buffer = deque(maxlen=capacity)
 
-    def add(self, state: int, action: int, reward: float, next_state: int, done: bool):
+    def push(self, state: int, action: int, reward: float, next_state: int, done: bool):
         """
         Store a single transition in the replay buffer
         """
@@ -24,3 +24,9 @@ class ReplayBuffer:
         Randomly sample a batch of stored transitions from the replay buffer
         """
         return random.sample(self.buffer, batch_size)
+
+    def length(self):
+        """
+        Returns the current number of stored transitions in the buffer
+        """
+        return len(self.buffer)
