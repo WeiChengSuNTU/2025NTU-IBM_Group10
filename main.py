f4c0dddf327a63856267ff00b7640592e9e9fa26
kalvoada <kalvoada@fit.cvut.cz>
2025-08-14 15:23:43 +0800

    main script init

---
+++ main.py
@@ -0,0 +1,25 @@
+
+def main():
+    # --------------------------
+    # Hyperparameters
+    # --------------------------
+    alpha = 0.01  # learning rate
+    gamma = 0.99  # discount factor
+    epsilon = 0.3  # exploration rate
+    episodes = 200  # total episodes
+    max_steps = 50  # max steps per episode
+    n_tests = 10  # number of test episodes
+
+    # --------------------------
+    # Run Deep Q-Learning
+    # --------------------------
+    # TODO
+
+    # --------------------------
+    # Analysis and Visualization
+    # --------------------------
+    # TODO
+
+
+if __name__ == "__main__":
+    main()
